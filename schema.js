9799fefd687c64a4cc938db469bb964cf231ea3b
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2024-09-11 09:48:08 +0100

    changed to js file

---
+++ schema.js
@@ -0,0 +1,19 @@
+{
+    "name": "string",
+    "subject":  ["array of subjects"],
+    "likes": "number of likes",
+    "available": true,
+    "reviews": {
+        "client_id": "",
+        "comments": "",
+        "date":""
+    },
+    "Qualificatios": [],
+    "phoneNumber": "",
+    "email": "",
+    "socialMediaHandles": [],
+    "userId": "uuid",
+    "createdAt": "date user was created",
+    "location/area": "",
+    "coordinate": "" 
+}
