50898abd03181412db9f8c6a7295437e5cb3812e
Uko Uwatt <uwattuko@gmail.com>
2024-09-17 03:41:39 +0100

    added files

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -14,6 +14,7 @@ const ClientController = {
         try {
             const hashedPassword = sha1(password);
             const client = await db.init();
+            console.log('database initiated');
             // Check if client already exists
             const existingClient = await db.clientCollection.findOne({ email });
             if (existingClient) {
@@ -22,22 +23,26 @@ const ClientController = {
             // prepare query string for geocoding
             const query = `${streetNumber} ${streetName} ${city} ${state} ${country}`;
             const uri = encodeURI(`https://api.tomtom.com/search/2/geocode/${query}.json`);
-            const response = await axios.get(uri, {params: {key: process.env.TOMTOM_API_KEY}});
+            console.log('uri:', uri);
+            const response = await axios.get(uri, {params: {key: process.env.DB_tomtomApiKey}});
+            console.log('response.data:', response.data);
             const results = response.data.results;
             if (!results && results.length === 0) {
                 return res.status(400).json({ message: 'Invalid address' });
             }
             let bestMatch = results[0];
-            results.forEach(result => {
-                if (result.matchConfidence && result.matchConfidence > bestMatch.matchConfidence) {
-                    bestMatch = result;
-                }
-            });
+            console.log('bestMatch:', bestMatch);
+            //results.forEach(result => {
+                //if (result.matchConfidence.score && result.matchConfidence.score > bestMatch.matchConfidence.score) {
+                    //bestMatch = result;
+                //}
+            //});
             const {lat, lon} = bestMatch.position;
+            console.log('lat:', lat);
             const location = {type: 'Point', coordinates: [lon, lat]};
             const address = bestMatch.address.freeformAddress;
             // Insert client data into database
-            clientData = {
+            const clientData = {
                 name,
                 email,
                 password: hashedPassword,
@@ -46,23 +51,24 @@ const ClientController = {
                 location,
                 address};
             const newClient = await db.clientCollection.insertOne(clientData);
+            console.log('newClient:', newClient);
             return res.status(201).json({ message: 'Client created successfully', clientId: newClient.insertedId });  
         } catch (error) {
-            return res.status(500).json({ message: `Error: ${error.message}` });
+            return res.status(500).json({ message: "Error" });
         } finally {
             await db.close();
         }
     },
 
     async deleteClient(req, res) {
-        const {clientId} = req.body;
-        if (!clientId) {
+        const {email, password} = req.body;
+        if (!email || !password) {
             return res.status(400).json({ message: 'Missing required field' });
         }
         try {
             const client = await db.init();
-            const objectId = new db.ObjectID(clientId);
-            const deletedClient = await db.clientCollection.deleteOne({_id: objectId});
+            //const objectId = new ObjectID(clientId);
+            const deletedClient = await db.clientCollection.deleteOne({ email });
             if (deletedClient.deletedCount === 0) {
                 return res.status(404).json({ message: 'Client not found' });
             }
