fa5e5221e77e248dacb8ac8ded486ea0881e45bc
Uko Uwatt <uwattuko@gmail.com>
2024-09-23 17:26:52 +0100

    corrected some endpoint names

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -2,8 +2,12 @@ import express from 'express';
 import ClientController from '../controllers/clientController.js';
 
 const router = express.Router();
-router.post('/createClient', ClientController.createClient);
-router.delete('/deleteClient', ClientController.deleteClient);
+router.post('/createClient', ClientController.createClient); //done
+router.delete('/deleteClient', ClientController.deleteClient); //done
+router.post('/clientLogin', ClientController.Login); //done
+router.delete('/clientLogout', ClientController.Logout); //done
+router.get('/getClient', ClientController.getClients); //done
+router.patch('/updateClient', ClientController.updateClient); //done
 
 export default router;
 
