6f06fb9d0eb1ec54a2372c7a3942f5140b0f4ab3
Uko Uwatt <uwattuko@gmail.com>
2024-09-23 17:27:52 +0100

    corrected syntax errors

---
+++ utils/redis.js
@@ -0,0 +1,12 @@
+import { createClient } from "redis";
+
+const redisClient = createClient();
+redisClient.on('error', error => console.log(`an error occured: ${error}`));
+redisClient.connect()
+    .then(
+        () => console.log('redis is connected')
+    )
+    .catch(
+        error => console.log(`redis connection issue: ${error}`)
+    );
+export default redisClient;
