0eac8071ba623fc659db8fff495da1419af22fa1
Uko Uwatt <uwattuko@gmail.com>
2024-09-23 17:27:27 +0100

    corrected some logic in the app

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import router from './routes/clientRoute.js';
+import session from 'express-session';
+import RedisStore from 'connect-redis';
+//import router from './routes/tutorsRoute.js';
+import clientRouter from './routes/clientRoute.js';
+import redisClient from './utils/redis.js';
 
 dotenv.config();
 const { SERVER_PORT } = process.env;
 const app = express();
 app.use(express.json());
-app.use(router);
-app.all('/', (req, res) => {
+app.use(express.urlencoded({ extended:true }));
+app.use(clientRouter);
+//app.use('/client', clientRouter);
+app.get('/', (req, res) => {
   res.send('welcome to parentPal\n');
 })
 app.listen(SERVER_PORT, () => {
-  console.log('server is running');
+  console.log(`server is runing on port ${SERVER_PORT}`);
 })
