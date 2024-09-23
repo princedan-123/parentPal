7e53ed569b46a3e4eabcd461fba514308203a039
Uko Uwatt <uwattuko@gmail.com>
2024-09-23 19:58:54 +0100

    added content to clientRoute.js

--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import appController from '../controllers/appController.js';
+import ClientController from '../controllers/clientController.js';
 
 const clientRouter = express.Router();
 clientRouter.get('/searchClient', appController.searchTutor);
+clientRouter.post('/createClient', ClientController.createClient); //done
+clientRouter.delete('/deleteClient', ClientController.deleteClient); //done
+clientRouter.post('/clientLogin', ClientController.Login); //done
+clientRouter.delete('/clientLogout', ClientController.Logout); //done
+clientRouter.get('/getClient', ClientController.getClients); //done
+clientRouter.patch('/updateClient', ClientController.updateClient); //done
+
 export default clientRouter;
